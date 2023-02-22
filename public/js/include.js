2d3b3ae3e5963eb1d17e42112053198bf86f4437
hiu-han <73466106+hiu-han@users.noreply.github.com>
2023-02-22 11:25:28 +0900

    230233 수 1125

--- public/js/include.js
+++ public/js/include.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $('#header').load('./include/header.html');
+  $('#fHeader').load('../include/header.html');
   $('#footer').load('./include/footer.html');
   $('#aside').load('./include/aside.html');
 });
