69403e5e7b8346f78a9161fee5e52cf6aa242573
hiu-han <73466106+hiu-han@users.noreply.github.com>
2023-03-03 16:50:34 +0900

    230303 금 1650

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,28 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  const NOWON_CLASSNAME = 'nowOn';
+
+  //
+  /* space info page -> 시설 안내 */
+  $('.info-sort__item .thumb__item').on('click', function () {
+    const spaceId = $(this).attr('id');
+
+    $('.info-sort__item .thumb__item').removeClass(NOWON_CLASSNAME);
+    $(this).addClass(NOWON_CLASSNAME);
+
+    $('.space-info__wrap')
+      .attr('class', '')
+      .addClass(`space-info__wrap ${spaceId}`);
+  });
+
+  //
+  /* 예약하기 페이지 -> 공간 선택 */
+  $('.space-btn__list .space-btn').on('click', function () {
+    const spaceId = $(this).attr('id');
+
+    $(this).addClass(NOWON_CLASSNAME).siblings().removeClass(NOWON_CLASSNAME);
+
+    $('.space-info__wrap')
+      .attr('class', '')
+      .addClass(`space-info__wrap ${spaceId}`);
+  });
+});
